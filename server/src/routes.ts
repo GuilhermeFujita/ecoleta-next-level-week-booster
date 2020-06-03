2bdf09325954b4f85049301c83e5cea547f83925
Guilherme Fujita <fujitaguilherme@gmail.com>
2020-06-02 23:35:48 -0300

    Refactor - Create point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import knex from "./database/connection";
 
+import PointsController from "./controllers/PointsController";
+
 const routes = express.Router();
+const pointsController = new PointsController();
 
 routes.get("/items", async (request, response) => {
 	const items = await knex("items").select("*");
@@ -17,43 +20,6 @@ routes.get("/items", async (request, response) => {
 	return response.json(serializedItems);
 });
 
-routes.post("/points", async (request, response) => {
-	const {
-		name,
-		email,
-		whatsapp,
-		latitude,
-		longitude,
-		city,
-		uf,
-		items,
-	} = request.body;
-
-	const trx = await knex.transaction();
-
-	const insertedIds = await trx("points").insert({
-		image: "image-fake",
-		name,
-		email,
-		whatsapp,
-		latitude,
-		longitude,
-		city,
-		uf,
-	});
-
-	const point_id = insertedIds[0];
-
-	const pointItems = items.map((item_id: number) => {
-		return {
-			item_id,
-			point_id,
-		};
-	});
-
-	await trx("point_items").insert(pointItems);
-
-	return response.json({ success: true });
-});
+routes.post("/points", pointsController.create);
 
 export default routes;
