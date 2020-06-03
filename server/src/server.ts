2ad95d2d8943fe6f9185659a797faeca139a7a40
Guilherme Fujita <fujitaguilherme@gmail.com>
2020-06-02 21:16:50 -0300

    Organizing routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-	console.log("Listagem de usuarios");
+app.use(express.json());
 
-	response.json(["Diego", "Cleiton", "Robson", "Daniel"]);
-});
+app.use(routes);
 
 app.listen(3333);
