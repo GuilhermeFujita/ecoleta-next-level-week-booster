3f9caffa4e511123481ca23ea00b215ab063dbda
Guilherme Fujita <fujitaguilherme@gmail.com>
2020-06-03 22:19:20 -0300

    Adding header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./Header";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header title="Ecoleta" />
+    </div>
+  );
 }
 
 export default App;
