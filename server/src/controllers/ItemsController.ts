1875b5a743f2e8288bef47b3fb468a399c777924
Guilherme Fujita <fujitaguilherme@gmail.com>
2020-06-04 22:42:28 -0300

    fixing api url typo

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
 			return {
 				id: item.id,
 				title: item.title,
-				image_url: `http://localhost:333/uploads/${item.image}`,
+				image_url: `http://localhost:3333/uploads/${item.image}`,
 			};
 		});
 
