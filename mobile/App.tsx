235002419acfae625a48404d07b840613c5c0934
Guilherme Fujita <fujitaguilherme@gmail.com>
2020-06-06 00:15:26 -0300

    Adding icons and Home Page structure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "react-native";
+
+import Home from "./src/pages/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Image } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Image source={require("../../assets/logo.png")} />
+    </View>
+  );
+};
+
+export default Home;
